d0ad87ac467443238928c979a781e5cf7d6b9791
Rijan2055 <rijandhakal2055@gmail.com>
2025-08-06 11:16:14 -0500

    Spelled New_York

--- modules/nixos/locale.nix
+++ modules/nixos/locale.nix
@@ -3,7 +3,7 @@
 
 {
   # Set your time zone.
-  time.timeZone = "America/New York";
+  time.timeZone = "America/New_York";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
